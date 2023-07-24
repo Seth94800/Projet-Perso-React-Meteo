aa29de6a3f4053282984ec7d5b09fc55dfc29668
Seth Gaara <83479322+Seth94800@users.noreply.github.com>
2023-07-24 04:20:06 +0200

    test commit

--- 2.react-weather/src/App.jsx
+++ 2.react-weather/src/App.jsx
@@ -43,7 +43,7 @@ function App() {
         <>
           <p className="city-name">{weatherData.city}</p>
           <p className="country-name">{weatherData.country}</p>
-          <p className="temperature">{weatherData.temperature}°c</p>
+          <p className="temperature">{weatherData.temperature}°c </p>
           <div className="info-icon-container">
             <img src={`/icons/${weatherData.iconId}.svg`} className="info-icon" alt="weather icon" />
           </div>
